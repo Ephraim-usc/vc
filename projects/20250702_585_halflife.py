9a25674bcbf67f280725f5d9f5330a1ddaa029b4
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2025-07-04 11:34:50 +0800

    Update 20250702_585_halflife.py

--- projects/20250702_585_halflife.py
+++ projects/20250702_585_halflife.py
@@ -20,10 +20,10 @@ def print_pk(system, filename, title):
   fig, ax = plt.subplots()
   
   ax.plot(ts, system.history['x', analytes, 'plasma']["sum"], label = "plasma total drug", color = "tab:red")
-  ax.plot(ts, system.history['x', analytes, 'tumor']["sum"], label = "tumor total drug", color = "tab:blue")
+  ax.plot(ts, system.history['x', analytes, 'tumor']["sum"], label = "tumor total drug", color = "tab:green")
   
   ax.plot(ts, system.history['x', analytes_drug, 'plasma']["sum"], label = "plasma uTCE", color = "tab:red", linestyle = "dashed")
-  ax.plot(ts, system.history['x', analytes_drug, 'tumor']["sum"], label = "tumor uTCE", color = "tab:blue", linestyle = "dashed")
+  ax.plot(ts, system.history['x', analytes_drug, 'tumor']["sum"], label = "tumor uTCE", color = "tab:green", linestyle = "dashed")
   
   ax.set_xlabel("time (d)")
   ax.set_xlim(0, 28)
@@ -41,6 +41,39 @@ def print_pk(system, filename, title):
   plt.gcf().set_size_inches(8, 8)
   fig.savefig(filename, dpi = 300)
   plt.close(fig)
+
+
+def print_tox(system, filename, title):
+  analytes_probody = system.get_all_analytes_of_drug(probody)
+  analytes_drug = system.get_all_analytes_of_drug(drug)
+  analytes = analytes_probody + analytes_drug
+  ts = [t/24 for t in system.history.data.keys()]
+  
+  fig, ax = plt.subplots()
+  
+  ax.plot(ts, system.history['x', analytes, 'SI']["sum"], label = "SI total drug", color = "tab:blue")
+  ax.plot(ts, system.history['x', analytes, 'lung']["sum"], label = "lung total drug", color = "tab:purple")
+  
+  ax.plot(ts, system.history['x', analytes_drug, 'SI']["sum"], label = "SI uTCE", color = "tab:red", linestyle = "blue")
+  ax.plot(ts, system.history['x', analytes_drug, 'lung']["sum"], label = "lung uTCE", color = "tab:blue", linestyle = "purple")
+  
+  ax.set_xlabel("time (d)")
+  ax.set_xlim(0, 28)
+  ax.axvspan(0, 7, alpha=0.1, color = "grey")
+  ax.axvspan(7, 14, alpha=0.2, color = "grey")
+  ax.axvspan(14, 28, alpha=0.3, color = "grey")
+  
+  ax.set_yscale("log")
+  ax.set_ylabel("conc. (nM)")
+  ax.set_ylim(1e-6, 1e3)
+  
+  ax.set_title(title)
+  ax.legend(loc = "upper right", prop={'size': 6})
+  fig.tight_layout()
+  plt.gcf().set_size_inches(8, 8)
+  fig.savefig(filename, dpi = 300)
+  plt.close(fig)
+
 
 
 drug = vc.Drug("TCE", 2)
@@ -78,13 +111,14 @@ system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "
 system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
 system.run(24*28*units.h, t_step = 1/6 * units.h, verbose = True)
 print_pk(system, "JANX007.png", "JANX007")
+print_tox(system, "tox_JANX007.png", "JANX007")
 
 system = vc.InvivoSystem(plasma, lymph, [tumor, lung, liver, SI, spleen, other], [cell_lung, cell_SI, Tn, Teff, cell_PRAD, cell_spleen], [probody, drug], [], halflives = [168*units.h, 1*units.h])
 system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "lung", "other"], [0.3/units.d, 0.05/units.d, 0.05/units.d, 0.05/units.d, 0.05/units.d, 0.05/units.d])
 system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
 system.run(24*28*units.h, t_step = 1/6 * units.h, verbose = True)
 print_pk(system, "JANX007_halflife168.png", "JANX007 (t1/2 = 168h)")
-
+print_tox(system, "tox_JANX007_halflife168.png", "JANX007 (t1/2 = 168h)")
 
 
 
@@ -124,13 +158,14 @@ system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "
 system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
 system.run(24*28*units.h, t_step = 1/6 * units.h, verbose = True)
 print_pk(system, "VIB585.png", "VIB585")
+print_tox(system, "tox_VIB585.png", "VIB585")
 
 system = vc.InvivoSystem(plasma, lymph, [tumor, lung, liver, SI, spleen, other], [cell_lung, cell_SI, Tn, Teff, cell_PRAD, cell_spleen], [probody, drug], [], halflives = [168*units.h, 1*units.h])
 system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "lung", "other"], [0.5/units.d, 0.01/units.d, 0.02/units.d, 0.02/units.d, 0.02/units.d, 0.02/units.d])
 system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
 system.run(24*28*units.h, t_step = 1/6 * units.h, verbose = True)
 print_pk(system, "VIB585_halflife168.png", "VIB585 (t1/2 = 168h)")
-
+print_tox(system, "tox_VIB585_halflife168.png", "VIB585 (t1/2 = 168h)")
 
 
 
@@ -169,12 +204,14 @@ system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "
 system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
 system.run(24*28*units.h, t_step = 1/6 * units.h, verbose = True)
 print_pk(system, "VIB585_int02.png", "VIB585 (int = 0.02/h")
+print_tox(system, "tox_VIB585_int02.png", "VIB585 (int = 0.02/h")
 
 system = vc.InvivoSystem(plasma, lymph, [tumor, lung, liver, SI, spleen, other], [cell_lung, cell_SI, Tn, Teff, cell_PRAD, cell_spleen], [probody, drug], [], halflives = [168*units.h, 1*units.h])
 system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "lung", "other"], [0.5/units.d, 0.01/units.d, 0.02/units.d, 0.02/units.d, 0.02/units.d, 0.02/units.d])
 system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
 system.run(24*28*units.h, t_step = 1/6 * units.h, verbose = True)
 print_pk(system, "VIB585_int02_halflife168.png", "VIB585 (int = 0.02/h, t1/2 = 168h)")
+print_tox(system, "tox_VIB585_int02_halflife168.png", "VIB585 (int = 0.02/h, t1/2 = 168h)")
 
 
 
@@ -214,13 +251,14 @@ system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "
 system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
 system.run(24*28*units.h, t_step = 1/6 * units.h, verbose = True)
 print_pk(system, "VIB585_int00.png", "VIB585 (int = 0.0/h)")
+print_tox(system, "tox_VIB585_int00.png", "VIB585 (int = 0.0/h)")
 
 system = vc.InvivoSystem(plasma, lymph, [tumor, lung, liver, SI, spleen, other], [cell_lung, cell_SI, Tn, Teff, cell_PRAD, cell_spleen], [probody, drug], [], halflives = [168*units.h, 1*units.h])
 system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "lung", "other"], [0.5/units.d, 0.01/units.d, 0.02/units.d, 0.02/units.d, 0.02/units.d, 0.02/units.d])
 system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
 system.run(24*28*units.h, t_step = 1/6 * units.h, verbose = True)
 print_pk(system, "VIB585_int00_halflife168.png", "VIB585 (int = 0.0/h, t1/2 = 168h)")
-
+print_tox(system, "tox_VIB585_int00_halflife168.png", "VIB585 (int = 0.0/h, t1/2 = 168h)")
 
 
 
@@ -259,9 +297,11 @@ system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "
 system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
 system.run(24*28*units.h, t_step = 1/6 * units.h, verbose = True)
 print_pk(system, "VIB585_without513.png", "VIB585 (without 513-BD)")
+print_tox(system, "tox_VIB585_without513.png", "VIB585 (without 513-BD)")
 
 system = vc.InvivoSystem(plasma, lymph, [tumor, lung, liver, SI, spleen, other], [cell_lung, cell_SI, Tn, Teff, cell_PRAD, cell_spleen], [probody, drug], [], halflives = [168*units.h, 1*units.h])
 system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "lung", "other"], [0.5/units.d, 0.01/units.d, 0.02/units.d, 0.02/units.d, 0.02/units.d, 0.02/units.d])
 system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
 system.run(24*28*units.h, t_step = 1/6 * units.h, verbose = True)
 print_pk(system, "VIB585_without513_halflife168.png", "VIB585 (without 513-BD, t1/2 = 168h)")
+print_tox(system, "tox_VIB585_without513_halflife168.png", "VIB585 (without 513-BD, t1/2 = 168h)")
