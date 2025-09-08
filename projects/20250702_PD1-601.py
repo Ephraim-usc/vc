1640c0c319750ac5fdd0a78c9b7f5306b484f666
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2025-09-08 16:52:30 +0800

    Update 20250702_PD1-601.py

--- projects/20250702_PD1-601.py
+++ projects/20250702_PD1-601.py
@@ -12,10 +12,11 @@ import numpy as np
 import pandas as pd
 
 
-drug = vc.Drug("drug", 3)
+drug = vc.Drug("unmasked", 3)
 drug.set_affinity(",,", 0, "PD1", 1*units.nM, 1e-4/units.s)
 drug.set_affinity(",,", 1, "CD25", 3*units.nM, 4e-4/units.s)
 drug.set_affinity(",,", 2, "CD132", 30*units.nM, 3e-3/units.s)
+drug.set_internalization([",,", ",,CD132", ",CD25,", ",CD25,CD132", "PD1,,", "PD1,,CD132", "PD1,CD25,", "PD1,CD25,CD132",], 0.1/units.h)
 
 # PD1对IL2R的CAB效应，按正常avidity计算
 drug.set_affinity("PD1,,", 1, "CD25", 1500/units.mm**2, 4e-4/units.s)
@@ -34,7 +35,55 @@ drug.set_affinity("PD1,CD25,", 2, "CD132", 150/units.mm**2, 3e-3/units.s)
 drug.set_affinity(",,CD132", 1, "CD25", 150/units.mm**2, 4e-4/units.s)
 drug.set_affinity("PD1,,CD132", 1, "CD25", 150/units.mm**2, 4e-4/units.s)
 
-drug.set_internalization([",,", ",,CD132", ",CD25,", ",CD25,CD132", "PD1,,", "PD1,,CD132", "PD1,CD25,", "PD1,CD25,CD132",], 0.1/units.h)
-drug.set_synapse_probability(0.1)
+
+
+probody = vc.Drug("masked", 3)
+probody.set_affinity(",,", 0, "PD1", 1*units.nM, 1e-4/units.s)
+probody.set_affinity(",,", 1, "CD25", math.inf*units.nM, 4e-4/units.s)
+probody.set_affinity(",,", 2, "CD132", math.inf*units.nM, 3e-3/units.s)
+probody.set_internalization([",,", ",,CD132", ",CD25,", ",CD25,CD132", "PD1,,", "PD1,,CD132", "PD1,CD25,", "PD1,CD25,CD132",], 0.1/units.h)
+
+# PD1对IL2R的CAB效应，按正常avidity计算
+probody.set_affinity("PD1,,", 1, "CD25", math.inf/units.mm**2, 4e-4/units.s)
+probody.set_affinity("PD1,,", 2, "CD132", math.inf/units.mm**2, 3e-3/units.s)
+
+# IL2R对PD1的CAB效应，按正常avidity计算
+probody.set_affinity(",CD25,", 0, "PD1", 500/units.mm**2, 1e-4/units.s)
+probody.set_affinity(",,CD132", 0, "PD1", 500/units.mm**2, 1e-4/units.s)
+probody.set_affinity(",CD25,CD132", 0, "PD1", 500/units.mm**2, 1e-4/units.s)
+
+# 结合CD25后，对CD132亲和力增强100倍
+probody.set_affinity(",CD25,", 2, "CD132", math.inf/units.mm**2, 3e-3/units.s)
+probody.set_affinity("PD1,CD25,", 2, "CD132", math.inf/units.mm**2, 3e-3/units.s)
+
+# 结合CD132后，对CD25亲和力增强10倍
+probody.set_affinity(",,CD132", 1, "CD25", math.inf/units.mm**2, 4e-4/units.s)
+probody.set_affinity("PD1,,CD132", 1, "CD25", math.inf/units.mm**2, 4e-4/units.s)
+
+
+system = vc.InvivoSystem(plasma, lymph, [tumor, lung, liver, SI, spleen, other], [Tn, Teff], [probody, drug], [], halflives = [20*units.d, 20*units.d])
+system.add_isodrug_transform(probody, drug, ["tumor", "plasma", "lymph", "SI", "lung", "other"], [0.094/units.d, 0.017/units.d, 0.017/units.d, 0.017/units.d, 0.017/units.d, 0.017/units.d])
+system.add_drug_dose(drug, 3*units.mg/units.kg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
+system.run(24*7*units.h, t_step = 1/6 * units.h, verbose = True)
+system.add_drug_dose(drug, 3*units.mg/units.kg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
+system.run(24*7*units.h, t_step = 1/6 * units.h, verbose = True)
+system.add_drug_dose(drug, 3*units.mg/units.kg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
+system.run(24*7*units.h, t_step = 1/6 * units.h, verbose = True)
+system.add_drug_dose(drug, 3*units.mg/units.kg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
+system.run(24*7*units.h, t_step = 1/6 * units.h, verbose = True)
+
+
+
+
+system.add_drug_dose(probody, 0.3*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
+system.run(24*7*units.h, t_step = 1/6 * units.h, verbose = True)
+system.add_drug_dose(probody, 1.5*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
+system.run(24*7*units.h, t_step = 1/6 * units.h, verbose = True)
+system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
+system.run(24*7*units.h, t_step = 1/6 * units.h, verbose = True)
+system.add_drug_dose(probody, 6*units.mg, molecular_weight = 100*units.kDa, body_weight = 70*units.kg)
+system.run(24*7*units.h, t_step = 1/6 * units.h, verbose = True)
+
+
 
 
