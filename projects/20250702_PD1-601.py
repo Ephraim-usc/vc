b4f575b7581b7bff537783158337a0711ca70ab9
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2025-09-08 18:04:58 +0800

    Update 20250702_PD1-601.py

--- projects/20250702_PD1-601.py
+++ projects/20250702_PD1-601.py
@@ -77,20 +77,20 @@ probody.set_affinity("PD1,,CD132", 1, "CD25", np.inf/units.um**2, 4e-4/units.s)
 drug = vc.Drug("unmasked", 3)
 drug.set_affinity(",,", 0, "PD1", 1*units.nM, 1e-4/units.s)
 drug.set_affinity(",,", 1, "CD25", np.inf*units.nM, 4e-4/units.s)
-drug.set_affinity(",,", 2, "CD132", 30*units.nM, 3e-3/units.s)
+drug.set_affinity(",,", 2, "CD132", 136*units.nM, 2e-2/units.s)
 drug.set_internalization([",,", ",,CD132", ",CD25,", ",CD25,CD132", "PD1,,", "PD1,,CD132", "PD1,CD25,", "PD1,CD25,CD132",], 0.1/units.h)
 
 # PD1对IL2R的CAB效应，按正常avidity计算
 drug.set_affinity("PD1,,", 1, "CD25", np.inf/units.um**2, 4e-4/units.s)
-drug.set_affinity("PD1,,", 2, "CD132", 15000/units.um**2, 3e-3/units.s)
+drug.set_affinity("PD1,,", 2, "CD132", 136/units.um**2, 2e-2/units.s)
 
 # IL2R对PD1的CAB效应，按正常avidity计算
 drug.set_affinity(",CD25,", 0, "PD1", 500/units.um**2, 1e-4/units.s)
 drug.set_affinity(",,CD132", 0, "PD1", 500/units.um**2, 1e-4/units.s)
 
 # 结合CD25后，对CD132亲和力增强100倍
-drug.set_affinity(",CD25,", 2, "CD132", 150/units.um**2, 3e-3/units.s)
-drug.set_affinity("PD1,CD25,", 2, "CD132", 150/units.um**2, 3e-3/units.s)
+drug.set_affinity(",CD25,", 2, "CD132", 136/units.um**2, 3e-3/units.s)
+drug.set_affinity("PD1,CD25,", 2, "CD132", 136/units.um**2, 3e-3/units.s)
 drug.set_affinity(",CD25,CD132", 0, "PD1", 500/units.um**2, 1e-4/units.s)
 
 # 结合CD132后，对CD25亲和力增强10倍
